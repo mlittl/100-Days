6d2af1898f9f146f34e2c444b84a75d58bf3a5ff
mlittl <matt.little01@gmail.com>
2024-01-19 10:55:55 -0400

    Updated filetree

--- day2/first.py
+++ day2/first.py
@@ -1,4 +1,9 @@
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+str(two_digit_number)
+firstnum = int(two_digit_number[0])
+secondnum = int(two_digit_number[1])
+
+print(firstnum+secondnum)
