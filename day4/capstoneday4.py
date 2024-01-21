84eb7dd9a4a659fc8f898617f37527edab2d96c7
mlittl <matt.little01@gmail.com>
2024-01-21 12:41:44 -0400

    Finished day4 capstone

--- day4/capstoneday4.py
+++ day4/capstoneday4.py
@@ -1,27 +1,58 @@
 import random as r
 
 def user_choice(rock,paper,scissors):
-    user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-    if user_choice == 0:
+    choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+    if choice == 0:
         print(rock)
-    elif user_choice == 1:
+    elif choice == 1:
         print(paper)
-    elif user_choice == 2:
+    elif choice == 2:
         print(scissors)
     else:
         print("You typed an invalid number, you lose!")
-    return user_choice
+    return choice
 
 def computer_choice(rock,paper,scissors):
-    computer_choice = r.randint(0,2)
+    choice = r.randint(0,2)
     print("Computer chose:")
-    if computer_choice == 0:
+    if choice == 0:
         print(rock)
-    elif computer_choice == 1:
+    elif choice == 1:
         print(paper)
-    elif computer_choice == 2:
+    elif choice == 2:
         print(scissors)
-    return computer_choice
+    return choice
+
+
+def print_victory_condition(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("It's a draw!")
+    elif (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
+        print("""
+        Congratulations! You are the champion!
+         ___________
+        '._==_==_=_.'
+        .-\:      /-.
+       | (|:.     |) |
+        '-|:.     |-'
+          \::.    /
+           '::. .'
+             ) (
+           _.' '._
+         |__WINNER__|
+        """)
+    elif user_choice == 2 and computer_choice == 0 or (user_choice == 0 and computer_choice == 1) or (user_choice == 1 and computer_choice == 2):
+        print("""
+        _____      
+        |   |
+        | L |     
+        | O |     
+        | S |     
+        | E | 
+        | R |_____
+        |_________|      
+        """)
+    return
 
 def main():
     
@@ -49,9 +80,9 @@ def main():
         (____)
     ---.__(___)
     '''
-    user_choice(rock,paper,scissors)
-    computer_choice(rock,paper,scissors)
-    
+    user = user_choice(rock,paper,scissors)
+    computer = computer_choice(rock,paper,scissors)
+    print_victory_condition(user, computer)
 
 if __name__ == "__main__":
     main()
