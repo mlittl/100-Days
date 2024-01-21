50f4cd4969c002c0e23d8c790ee09483d08467ac
mlittl <matt.little01@gmail.com>
2024-01-21 12:02:05 -0400

    Got the user choice and computer choice working with the Ascii art

--- day4/capstoneday4.py
+++ day4/capstoneday4.py
@@ -1,29 +1,57 @@
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+import random as r
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
+def user_choice(rock,paper,scissors):
+    user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+    if user_choice == 0:
+        print(rock)
+    elif user_choice == 1:
+        print(paper)
+    elif user_choice == 2:
+        print(scissors)
+    else:
+        print("You typed an invalid number, you lose!")
+    return user_choice
 
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
+def computer_choice(rock,paper,scissors):
+    computer_choice = r.randint(0,2)
+    if computer_choice == 0:
+        print(rock)
+    elif computer_choice == 1:
+        print(paper)
+    elif computer_choice == 2:
+        print(scissors)
+    return computer_choice
 
-#Write your code below this line 👇
+def main():
+    
+    rock = '''
+        _______
+    ---'   ____)
+        (_____)
+        (_____)
+        (____)
+    ---.__(___)
+    '''
+    paper = '''
+        _______
+    ---'   ____)____
+            ______)
+            _______)
+            _______)
+    ---.__________)
+    '''
+    scissors = '''
+        _______
+    ---'   ____)____
+            ______)
+        __________)
+        (____)
+    ---.__(___)
+    '''
+    user_choice(rock,paper,scissors)
+    print(f"Computer chose:\n")
+    computer_choice(rock,paper,scissors)
+    
 
+if __name__ == "__main__":
+    main()
