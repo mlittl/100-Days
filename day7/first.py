7649ff3ba547511f0d3dbd8816e3c455edac941f
mlittl <matt.little01@gmail.com>
2024-01-25 08:43:51 -0400

    Finished Day 7

--- day7/first.py
+++ day7/first.py
@@ -1,4 +1,3 @@
-# Step 1 
 import random
 import sys
 
@@ -7,18 +6,23 @@ def player_guess():
     guessing = guessing.lower()
     return guessing
 
+def join(word_to_join):
+    wordjoin = ""
+    for item in word_to_join:
+        wordjoin += item
+    return wordjoin
+
 def guess_checking(guess, word_choice):
     guess_check = False
     while True:
-        if len(guess) > 1 or len(guess) < 0 or guess != str:
+        if len(guess) > 1:
             print("You must guess a single letter")
             break
-        if guess == "":
-            print("You must guess a single letter")
+        if guess == " ":
+            print("You must guess a single letter spaces")
             break
         elif guess in word_choice:
             guess_check = True
-            print("You got one...\n")
             return guess_check
         else:
             print("Nope! Hahaha!\n")
@@ -41,61 +45,110 @@ def setup_letterlist(word_choice):
         letter_list.append(letters)
     return letter_list
 
-def victory_check(guesses_dict, word_choice):
-    if guesses_dict >= 5:
-        print("Game Over! Man has Hang'd")
-        sys.exit()
-
-
-def print_hangman(spaces):
-    
+def print_hangman(spaces, lives):
     gallows = [
-        "  _______",
-        "  |     ",
-        "  |     ",
-        "  |     ",
-        "  |     ",
-        "__|__"
-    ]
-    for line in gallows:
-        print(line)    
-    spacesjoined = ""
-    for item in spaces:
-        spacesjoined += item
-    print(spacesjoined)
+    '''   
+          _______
+          |     |
+          |     
+          |     
+          |
+          |     
+        __|__\n''',
+        
+    '''   
+          _______
+          |     |
+          |     0
+          |     
+          |     
+          |
+        __|__\n''',
 
+    '''   
+          _______
+          |     |
+          |     0
+          |     |
+          |     |
+          |       
+          |
+        __|__\n''',
+
+    '''   
+          _______
+          |     |
+          |     0
+          |     |
+          |     |
+          |    /  
+          |
+        __|__\n''',
+
+    '''   
+          _______
+          |     |
+          |     0
+          |     |
+          |     |
+          |    / \ 
+          |
+        __|__\n''',
+
+    '''   
+          _______
+          |     |
+          |     0
+          |    /|
+          |     |
+          |    / \ 
+          |
+        __|__\n''',
     
+    '''   
+          _______
+          |     |
+          |     0
+          |    /|\ 
+          |     |
+          |    / \ 
+          |
+        __|__
+        
+        ### GAME OVER ###
+        \n'''
+    ]
+    print(gallows[lives])
+    spacesjoined = join(spaces)
+    print(spacesjoined)
+    return gallows[lives + 1]
 
 def main():
     print("Welcome to Hangman")
     word_choice = loadword()
     spaces = setup_spaces(word_choice)
     letter_list = setup_letterlist(word_choice)
-    winloss_counter = 0
+    lives = 0
     guesses_count = 0
     guesses_dict = {}
     while True:
-        print_hangman(spaces)
+        gameoverchecklist = print_hangman(spaces, lives)
         guess = player_guess()
         guesses_count += 1
-        guesses_dict[guess] = guesses_count
         if guess_checking(guess, word_choice) == True:
-            for letters in range(len(letter_list)):
+            guesses_dict[guess] = guesses_count
+            for letters in range(len(letter_list)): # Replaces the spaces with letters upon successful guess
                 if guess in letter_list[letters]:
                     spaces[letters] = letter_list[letters]
-                    if spaces == letter_list:
+                    if spaces == letter_list: # Win Condition
                         print("You win!")
                         sys.exit()
         else:
-            winloss_counter += 1
-            if winloss_counter == 5:
-                print("GAME OVER")
-                break
-            print_hangman(spaces)
-
-
-
-
+            lives += 1
+            if lives >= 6:
+                gameovercheck = join(gameoverchecklist)
+                print(gameovercheck)
+                sys.exit()
 
 if __name__ == "__main__":
     main()
