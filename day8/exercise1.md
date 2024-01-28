18f28d6d6080d4cf451e688a14afcf654e85a49b
mlittl <matt.little01@gmail.com>
2024-01-27 20:05:02 -0400

    Finished first exercise

---
+++ day8/exercise1.md
@@ -0,0 +1,5 @@
+# First exercise
+Review: 
+Create a function called greet(). 
+Write 3 print statements inside the function.
+Call the greet() function and run your code.
---
+++ day8/first.py
@@ -0,0 +1,12 @@
+
+
+def greet():
+    print("Hello there!")
+    print("Greetings!")
+    print("Why, welcome, stranger!")
+
+def main():
+    greet() 
+
+if __name__ == "__main__":
+    main()
